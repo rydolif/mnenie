160647d2434677455b9ee85599d8a3acfb502b99
rydolif <rudolifrudolif@gmail.com>
2018-07-24 18:40:34 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,95 @@
+$(function() {
+
+//------------------------------гамбургер-----------------------------
+$('.hamburger').click(function() {
+  $(this).toggleClass('hamburger-active');
+  $('nav').toggleClass('nav-active');
+  $('header').toggleClass('header-menu');
+});
+
+//-------------------------------попандер---------------------------------------
+  $('.modal').popup({transition: 'all 0.3s'});
+
+//------------------------------------form-------------------------------------------
+	$('input[type="tel"]').mask('+0 (000) 000-00-00');
+
+	jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+	   return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+	}, "Введите Ваш телефон");
+
+  $(".order-form").validate({
+    messages: {
+      name: "Введите ваше Имя",
+      phone: "Введите ваш телефон",
+    },
+    rules: {
+      "phone": {
+        required: true,
+        phoneno: true
+      }
+    },
+    submitHandler: function(form) {
+      var t = {
+        name: jQuery(".order-form").find("input[name=name]").val(),
+        phone: jQuery(".order-form").find("input[name=phone]").val(),
+        number: jQuery(".order-form").find("input[name=number]").val(),
+        product: jQuery(".order-form").find("input[name=product]").val(),
+        subject: jQuery(".order-form").find("input[name=subject]").val()
+      };
+      ajaxSend('.order-form', t);
+    }
+  });
+
+  $(".consultation-form").validate({
+    messages: {
+      name: "Введите ваше Имя",
+      phone: "Введите ваш телефон",
+    },
+    rules: {
+      "phone": {
+        required: true,
+        phoneno: true
+      }
+    },
+    submitHandler: function(form) {
+      var t = {
+        name: jQuery(".consultation-form").find("input[name=name]").val(),
+        phone: jQuery(".consultation-form").find("input[name=phone]").val(),
+        subject: jQuery(".consultation-form").find("input[name=subject]").val()
+      };
+      ajaxSend('.consultation-form', t);
+    }
+  });
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: "POST",
+      url: "sendmail.php",
+      data: data,
+      success: function() {
+        $(".modal").popup("hide");
+        $("#thanks").popup("show");
+        setTimeout(function() {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
+
+//----------------------------------------fixed----------------------------------
+  $(window).scroll(function(){
+      if($(this).scrollTop()>20){
+          $('.header').addClass('header-active');
+      }
+      else if ($(this).scrollTop()<20){
+          $('.header').removeClass('header-active');
+      }
+  });
+
+});
+
+//----------------------------------------preloader----------------------------------
+
+$(window).on('load', function(){
+  $('.preloader').delay(1000).fadeOut('slow');
+});
