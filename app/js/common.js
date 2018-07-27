35675fe68e13f13978ec89900db3dce07c956860
rydolif <rudolifrudolif@gmail.com>
2018-07-27 10:02:35 +0300

    правки

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,5 @@
 $(function() {
 
-//------------------------------гамбургер-----------------------------
-// $('.hamburger').click(function() {
-//   $(this).toggleClass('hamburger-active');
-//   $('nav').toggleClass('nav-active');
-//   $('header').toggleClass('header-menu');
-// });
-
 //-------------------------------попандер---------------------------------------
   $('.page-modal').popup({transition: 'all 0.3s'});
 
@@ -36,7 +29,7 @@ $(function() {
       submitHandler: function(form) {
         var t = {
           name: jQuery('.form-' + index).find("input[name=name]").val(),
-          phone: jQuery('.form-' + index).find("input[name=phone]").val(),
+          phone: jQuery('.form-' + index).find("input[name=tel]").val(),
           subject: jQuery('.form-' + index).find("input[name=subject]").val()
         };
         ajaxSend('.form-' + index, t);
@@ -82,95 +75,3 @@ $(function() {
 $(window).on('load', function(){
   $('.preloader').delay(1000).fadeOut('slow');
 });
-
-//-------------------------------------------------------------------------
-$(document).ready(function() {
-    var coopLink = $('body').find('.coop').find('a'),
-        coop = $('body').find('.coop');
-    $(coop).css({
-        color: '#4C4C4C',
-    });
-    $(coopLink).css({
-        color: '#4C4C4C',
-    });
-    $(coopLink).html('Сайт разработан студией Romi');
-    $(".phone").addClass("hidden-lg");
-    $(".phone").addClass("hidden-md");
-    $("footer a[href='price.html']").before("<a href='tel:+74992701299' class = 'footer_phone'>+7 (499) 270-12-99</a><br><a href='tel:+88001003254' class = 'footer_phone'>8 (800) 100-32-54</a><p class = 'footer_phone'> (Звонок по России БЕСПЛАТНЫЙ)</p>  <p class = 'footer_phone'><b>Почта:</b> info@ok-mnenie.ru </p>");
-    $("a[href='about.html'], a[href='/about.html']").html('<i class="i6"></i><strong>Контакты</strong>');
-    $('.main-nav menu, .mobile-menu').append('<a class="menu-item" href="/blog2/"><i class="i8"></i><strong>Блог</strong></a>');
-    
-    pageTitle = document.title;
-    $("#promo .tel").mask("+7 (999) 999-99-99");
-});
-
-var emptyPhone = true;
-var emptyTel = true;
-$('#phone').keydown(function(e) {
-    
-    if (emptyPhone && (e.which == 56 || e.which == 104)) {
-        e.preventDefault();
-        return false;
-    }
-    emptyPhone = false;
-    
-});
-
-$('#promo .tel').keydown(function(e) {
-    
-    if (emptyTel && (e.which == 56 || e.which == 104)) {
-        e.preventDefault();
-        return false;
-    }
-    emptyTel = false;
-    
-});
-
-
-var pageTitle = '';
-
-function titleAlert() {
-    if (document.title == pageTitle) {
-        document.title = 'Внимание акция';
-    } else {
-        document.title = pageTitle;
-    }
-}
-
-var timeout = 65;
-var activityTimeout = setTimeout(inActive, timeout * 1000);
-var titleInterval;
-var stop = false;
-
-function resetActive(){
-    $('#promo').removeClass('uptocall-blur');
-    if (stop) {
-        return false;
-    }
-    document.title = pageTitle;
-    clearTimeout(activityTimeout);
-    //clearInterval(titleInterval);
-    activityTimeout = setTimeout(inActive, timeout * 1000);
-}
-
-function inActive(){
-    titleInterval = setInterval(titleAlert, 1000);
-    $('#promo').dialog({width: 500,height:250}); 
-    $('#promo .tel').blur();
-    stop = true;
-}
-
-$(document).bind('mousemove', function(){resetActive()});
-
-$('#promo .send').click(function(){
-    $tel = $('#promo .tel').val();
-    if ($tel == '') {
-        $('#promo .tel').focus();
-        return false;
-    }
-    $.post("promo.php", {phone: $tel}, function() {
-        stop = true;
-        $('#promo').dialog('close');
-    });
-});
-
